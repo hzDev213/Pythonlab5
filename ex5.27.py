3811abb743abc974526c569deec04acfb9afbc19
213 <107265321+hzDev213@users.noreply.github.com>
2024-12-05 11:57:25 +0300

    Update ex5.27.py

--- ex5.27.py
+++ ex5.27.py
@@ -1,11 +1,9 @@
 
-for i in range(10000,100001):
-    pi= 3.14
-    piCount = 1
-    j = 1
-    sum = 1
-    for j in range (j,i):
-        piCount= ((-1)**(j+1)/(2*j+1))
-        sum += piCount
-    pi *= sum
+for i in range(10000,100001,10000):
+    print("Iterations",i)
+    pi= 0
+    
+    for j in range (1,i+1,1):
+        pi= ((-1)**(i+1)/(2*i+1))
+    pi *= 4
     print(pi)
